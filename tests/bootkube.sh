568d37b7323a198eea22f81e1f34f87b02053529
Chris <chris.moirano@gmail.com>
2018-11-21 06:39:49 +0000

    add cache clear

--- tests/bootkube.sh
+++ tests/bootkube.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # Copied from Aurojit Panda's lab2-raft code
 set -E
+
+
 if ! sudo minikube status > /dev/null; then 
+    if [ $# -gt 0 ]; then
+        sudo rm -rI $HOME/.kube/cache
+    fi
     sudo minikube start --vm-driver=none
 else
     echo 'Minikube is already running. Use `sudo minikube stop` to stop it if necessary'
