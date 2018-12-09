7392902dafda6fba56f607c75e805e6044536679
yair-schiff <yzs208@nyu.edu>
2018-12-09 02:25:29 +0000

    Collect metrics on finger table entry updating

--- chord/metric.go
+++ chord/metric.go
@@ -9,6 +9,7 @@ import (
 )
 
 const REQUSTMETRIC = 1
+const FINGERMETRIC = 2
 
 // METRICSFILE represents where we will write our JSON metrics
 const METRICSFILE = "/tmp/node%v.JSON"
@@ -22,6 +23,11 @@ func (t JSONTime) MarshalJSON() ([]byte, error) {
 	return []byte(stamp), nil
 }
 
+// Metric -> interface to be implemented by both RequestMetric and FingerMetric
+type Metric interface {
+	metricType() uint32
+}
+
 //RequestMetric -> object that we will use in chord to gather metrics on requests
 type RequestMetric struct {
 	Class      uint32
@@ -33,10 +39,28 @@ type RequestMetric struct {
 	End        JSONTime
 }
 
+// metricType implementation for RequestMetric
+func (rm RequestMetric) metricType() uint32 {
+	return rm.Class
+}
+
+// FingerMetric -> object that we will use in chord to gather metrics on number
+//of fingers that had to be fixed
+type FingerMetric struct {
+	Class        uint32
+	SourceNode   uint64
+	FixedFingers uint64
+}
+
+// metricType implementation for FingerMetric
+func (fm FingerMetric) metricType() uint32 {
+	return fm.Class
+}
+
 // gatherMetrics will act as a seperate thread to perodically read metric objects inserted from
 // our main chord server and process
 // Take a channel of request metrics and appends them to a JSON file
-func gatherMetrics(metricCh <-chan RequestMetric, nodeID uint64) {
+func gatherMetrics(metricCh <-chan Metric, nodeID uint64) {
 	filename := fmt.Sprintf(METRICSFILE, nodeID)
 
 	file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
@@ -44,7 +68,7 @@ func gatherMetrics(metricCh <-chan RequestMetric, nodeID uint64) {
 		log.Fatalf(red("%v"), err)
 	}
 	defer file.Close()
-	var metric RequestMetric
+	var metric Metric
 	for len(metricCh) > 0 {
 		metric = <-metricCh
 		blob, err := json.Marshal(metric)
